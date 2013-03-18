a4621b78f731c025badc34d3c0f31c683f13cd14
Ramie Blatt <rblatt@warmlyyours.com>
2013-03-17 21:45:59 -0600

    exclude guest or anonymous users

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -1,6 +1,6 @@
 User.class_eval do 
   after_create :send_signup_confirmation
   def send_signup_confirmation
-    UserMailer.signup_email(self).deliver
+    UserMailer.signup_email(self).deliver unless self.anonymous?
   end
 end 
